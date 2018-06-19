713c48b1721b551752a0af1e56ffbb69009a3104
a.mostovenko <a.mostovenko@smartweb.com.ua>
2018-06-19 18:01:25 +0300

    some sidebar info display

--- frontend/Sidebar/index.js
+++ frontend/Sidebar/index.js
@@ -1,7 +1,12 @@
 import React from "react";
 import { StoreContext } from "../store";
 import { withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import ListSubheader from '@material-ui/core/ListSubheader';
+import List from '@material-ui/core/List';
 import Paper from '@material-ui/core/Paper';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
 
 const styles = theme => ({
     paper: {
@@ -14,7 +19,21 @@ const styles = theme => ({
 
 const SidebarComponent = withStyles(styles)((props) => {
     const { classes } = props;
-    return <Paper className={classes.paper} >{ props.selectedModuleId }</Paper>
+    const module = props.modules.find((m) => m.id === props.selectedModuleId);
+    debugger;
+    return (
+        <Paper className={classes.paper}>
+            <Typography align="left" variant="title">{module.name}</Typography>
+            <Typography align="left" variant="subheading">
+                {module.reasons.length} Reasons:
+            </Typography>
+            <ul>
+                { module.reasons.map((reason) => {
+                    return <li><Typography align="left">{reason.module}</Typography></li>
+                })}
+            </ul>
+        </Paper>
+    )
 })
 
 const Sidebar = () => {
@@ -22,9 +41,10 @@ const Sidebar = () => {
         <StoreContext.Consumer>
             {
                 ctx => (
-                    <SidebarComponent
+                    ctx.selectedModuleId ? <SidebarComponent
+                        modules={ctx.statsData.modules || []}
                         selectedModuleId={ctx.selectedModuleId}
-                    />
+                    /> : null
                 )
             }
         </StoreContext.Consumer>
